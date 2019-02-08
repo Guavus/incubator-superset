1b4e4a60f1c2c6235059249a821fbc88463a8bd5
harshit.kumar <harshit.kumar@guavus.com>
2019-02-08 12:34:51 +0530

    removing create_whl.sh

--- create_whl.sh
+++
@@ -1,4 +0,0 @@
-# echo -e "# # # # # # # START : Creating Wheel file # # # # # # #"
-# pipenv install
-# pipenv run python ./setup.py bdist_wheel
-# echo -e "# # # # # # # END : Created Wheel file # # # # # # #"
