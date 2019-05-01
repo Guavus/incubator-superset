9f28e33ff123cbe30c54cb44d14aafbb60e2bc76
Jitendra Kumawat <Jitendra.Kumawat@guavus.com>
2019-05-01 13:16:26 +0530

    fix(viewquery):updated logic to display hive partitioned query in view query dialog box
    display hive partitioned query in view query dialog
    
    UIC-UIC-1245

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -492,6 +492,12 @@ class SqlaTable(Model, BaseDatasource):
         if query_obj['is_prequery']:
             query_obj['prequeries'].append(sql)
         sql = self.mutate_query_from_config(sql)
+
+        """Apply HIVE_QUERY_GENERATOR """
+        HIVE_QUERY_GENERATOR = config.get('HIVE_QUERY_GENERATOR')
+        if HIVE_QUERY_GENERATOR:
+            sql = HIVE_QUERY_GENERATOR(sql,query_obj,self.database,self.datasource_name)
+
         return QueryStringExtended(labels_expected=sqlaq.labels_expected, sql=sql)
 
     def get_query_str(self, query_obj):
@@ -832,10 +838,7 @@ class SqlaTable(Model, BaseDatasource):
         
         logging.info('[PERFORMANCE CHECK] SQL Query formation time {0} '.format(datetime.now() - qry_start_dttm))
         
-        """Apply HIVE_QUERY_GENERATOR """
-        HIVE_QUERY_GENERATOR = config.get('HIVE_QUERY_GENERATOR')
-        if HIVE_QUERY_GENERATOR:
-            sql = HIVE_QUERY_GENERATOR(sql,query_obj,self.database,self.datasource_name)
+        
 
         db_engine_spec = self.database.db_engine_spec
         try:
