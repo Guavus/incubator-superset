694b3a0b0dbf183917de3ae5ec78afeabb40bf80
ankur.singhal <ankur.singhal@guavus.com>
2019-05-20 15:59:30 +0530

    feat(publisher_subscriber):  review
    UIC-1321

--- superset/assets/src/dashboard/util/charts/getFormDataWithExtraFilters.js
+++ superset/assets/src/dashboard/util/charts/getFormDataWithExtraFilters.js
@@ -105,7 +105,6 @@ export default function getFormDataWithExtraFilters({
       dashboardMetadata,
       filters,
       sliceId,
-      filterKeys,
       linkedSlicesExistInFilters,
     }),
   };
