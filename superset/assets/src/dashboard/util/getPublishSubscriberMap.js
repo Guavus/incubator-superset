b54bf99bb66229e5dc7314d9e57893fdd004d4c7
rasmiranjan.nayak <rasmiranjan.nayak@guavus.com>
2019-05-29 14:39:48 +0530

    feature(modal-action): add modal action support in intra-dashboard communication
    formatted
    
    UIC-1497

--- superset/assets/src/dashboard/util/getPublishSubscriberMap.js
+++ superset/assets/src/dashboard/util/getPublishSubscriberMap.js
@@ -20,148 +20,148 @@ import * as _ from 'lodash';
 const APPLY_FILTER = 'APPLY_FILTER';
 
 function createPublishDataFor(slice, slices) {
-    return {
-        id: slice.id,
-        publish_columns: slice.formData.publish_columns,
-        subcribers: getSubsribersFor(slice.id, slices),
-        viz_type: slice.formData.viz_type
-    }
+  return {
+    id: slice.id,
+    publish_columns: slice.formData.publish_columns,
+    subcribers: getSubsribersFor(slice.id, slices),
+    viz_type: slice.formData.viz_type
+  }
 
 }
 
 function getSubsribersFor(publisherId, slices) {
-    var subs = [];
-    slices.forEach(element => {
-        if (isSubscriber(element) && isPublisherExistInLinkedSlices(element, publisherId)) {
-            subs.push(element.id);
-        }
-    });
-    return subs;
+  var subs = [];
+  slices.forEach(element => {
+    if (isSubscriber(element) && isPublisherExistInLinkedSlices(element, publisherId)) {
+      subs.push(element.id);
+    }
+  });
+  return subs;
 }
 
 function isPublisher(slice) {
-    return (slice.formData.publish_columns && slice.formData.publish_columns.length > 0)
+  return (slice.formData.publish_columns && slice.formData.publish_columns.length > 0)
 }
 
 function getPublisherMap(slices) {
-    var pubs;
-    slices.forEach(element => {
-        if (isPublisher(element)) {
-            if (!pubs) {
-                pubs = {};
-            }
-            pubs[element.id] = createPublishDataFor(element, slices);
-        }
-    });
-    return pubs;
+  var pubs;
+  slices.forEach(element => {
+    if (isPublisher(element)) {
+      if (!pubs) {
+        pubs = {};
+      }
+      pubs[element.id] = createPublishDataFor(element, slices);
+    }
+  });
+  return pubs;
 }
 
 function createFilterDataFromPublishColumns(obj) {
-    var filterList = [];
-    obj.publish_columns.forEach(element => {
-        filterList.push({
-            col: element,
-            op: 'in'
-        })
-    });
-    return filterList;
+  var filterList = [];
+  obj.publish_columns.forEach(element => {
+    filterList.push({
+      col: element,
+      op: 'in'
+    })
+  });
+  return filterList;
 }
 
 function isPublisherExistInLinkedSlices(slice, publisherId) {
-    var item = _.find(slice.formData.linked_slice, function (item) {
-        return ((Number.isInteger(item) && item == publisherId) ||
-            (item instanceof Object && item.publisher_id == publisherId))
-    })
-    return (item != undefined)
+  var item = _.find(slice.formData.linked_slice, function (item) {
+    return ((Number.isInteger(item) && item == publisherId) ||
+      (item instanceof Object && item.publisher_id == publisherId))
+  })
+  return (item != undefined)
 }
 
 function getLinkedSlices(slices, publishers) {
-    var ls = {};
-    slices.forEach(element => {
-        // backward compitable
-        if (Number.isInteger(element) && publishers && publishers.hasOwnProperty(element)) {
-            ls[element] = createFilterDataFromPublishColumns(publishers[element]);
-        }
-        // as per new ds
-        else if (element instanceof Object && publishers && publishers.hasOwnProperty(element.publisher_id)) {
-            ls[element.publisher_id] = element.subscribe_columns;
-        }
-    });
-    return ls;
+  var ls = {};
+  slices.forEach(element => {
+    // backward compitable
+    if (Number.isInteger(element) && publishers && publishers.hasOwnProperty(element)) {
+      ls[element] = createFilterDataFromPublishColumns(publishers[element]);
+    }
+    // as per new ds
+    else if (element instanceof Object && publishers && publishers.hasOwnProperty(element.publisher_id)) {
+      ls[element.publisher_id] = element.subscribe_columns;
+    }
+  });
+  return ls;
 }
 
 function createSubscriberDataFor(slice, publishers) {
-    return {
-        id: slice.id,
-        viz_type: slice.formData.viz_type,
-        actions: slice.formData.hasOwnProperty('actions') ? slice.formData.actions : [APPLY_FILTER],
-        linked_slices: getLinkedSlices(slice.formData.linked_slice, publishers),
-        extras: slice.formData.hasOwnProperty('extras') ? slice.formData.extras : undefined
-    }
+  return {
+    id: slice.id,
+    viz_type: slice.formData.viz_type,
+    actions: slice.formData.hasOwnProperty('actions') ? slice.formData.actions : [APPLY_FILTER],
+    linked_slices: getLinkedSlices(slice.formData.linked_slice, publishers),
+    extras: slice.formData.hasOwnProperty('extras') ? slice.formData.extras : undefined
+  }
 
 }
 
 function isSubscriber(slice) {
-    return (slice.formData.linked_slice && slice.formData.linked_slice.length > 0)
+  return (slice.formData.linked_slice && slice.formData.linked_slice.length > 0)
 }
 
 function getSubscriberMap(slices, publishers) {
-    var subs;
-    slices.forEach(element => {
-        if (isSubscriber(element)) {
-            if (!subs) {
-                subs = {};
-            }
-            subs[element.id] = createSubscriberDataFor(element, publishers)
-        }
-    });
-    return subs;
+  var subs;
+  slices.forEach(element => {
+    if (isSubscriber(element)) {
+      if (!subs) {
+        subs = {};
+      }
+      subs[element.id] = createSubscriberDataFor(element, publishers)
+    }
+  });
+  return subs;
 }
 
 export default function getPublishSubscriberMap(slices) {
-    slices = updateSlices(slices);
-    let publishers = getPublisherMap(slices);
-    let subscribers = publishers ? getSubscriberMap(slices, publishers) : undefined;
-    return {
-        publishers: publishers,
-        subscribers: subscribers
-    };
+  slices = updateSlices(slices);
+  let publishers = getPublisherMap(slices);
+  let subscribers = publishers ? getSubscriberMap(slices, publishers) : undefined;
+  return {
+    publishers: publishers,
+    subscribers: subscribers
+  };
 }
 
 function updateSlices(slices) {
-    let updatedSlices = _.clone(slices);
-    updatedSlices.forEach(slice => {
-      const linkedSlices = getLinkedSlicesFromSubscriberLayer(slice.formData.subscriber_layers);
-      slice.formData.linked_slice = linkedSlices ? linkedSlices : slice.formData.linked_slice;
-       slice.formData.actions = getUniqueActionsForSlice(slice.formData.subscriber_layers);
-       slice.formData.useAsModal =  slice.formData.actions && slice.formData.actions.indexOf('USE_AS_MODAL') >= 0 ? true : false;
-    });
+  let updatedSlices = _.clone(slices);
+  updatedSlices.forEach(slice => {
+    const linkedSlices = getLinkedSlicesFromSubscriberLayer(slice.formData.subscriber_layers);
+    slice.formData.linked_slice = linkedSlices ? linkedSlices : slice.formData.linked_slice;
+    slice.formData.actions = getUniqueActionsForSlice(slice.formData.subscriber_layers);
+    slice.formData.useAsModal = slice.formData.actions && slice.formData.actions.indexOf('USE_AS_MODAL') >= 0 ? true : false;
+  });
 
-    return updatedSlices;
+  return updatedSlices;
 }
 
 function getUniqueActionsForSlice(subscriberLayers) {
   let actions = [];
 
   if (subscriberLayers) {
-      subscriberLayers.forEach(element => {
-        actions = _.union(actions, element.actions);
-      });
+    subscriberLayers.forEach(element => {
+      actions = _.union(actions, element.actions);
+    });
   }
 
   return actions;
 }
 
 function getLinkedSlicesFromSubscriberLayer(subscriberLayer) {
-    let linkedSlices;
-    if (subscriberLayer) {
-        linkedSlices = [];
-        subscriberLayer.forEach(element => {
-            element.linked_slice.forEach(item => {
-                linkedSlices.push(item);
-            });
-        });
-    }
+  let linkedSlices;
+  if (subscriberLayer) {
+    linkedSlices = [];
+    subscriberLayer.forEach(element => {
+      element.linked_slice.forEach(item => {
+        linkedSlices.push(item);
+      });
+    });
+  }
 
-    return linkedSlices;
+  return linkedSlices;
 }
