cb3da7d0555dd4e7d29648e9ba60e74d1faccacc
Arpit Agarwal <Arpit.Agarwal@guavus.com>
2019-07-28 13:30:13 +0530

    feat(REST Actions):  some cleanup
    UIC-1824: #time 30m cleanup

--- superset/assets/src/utils/restActions.js
+++ superset/assets/src/utils/restActions.js
@@ -1,24 +1,24 @@
 export function createPostPayload(payloadformat, data) {
   let payload = JSON.parse(JSON.stringify(payloadformat)); // deepclone hack
-  replacePlaceholder(payload,data)
+  replacePlaceholder(payload, data)
   return payload;
 }
 
-const replacePlaceholder = (obj ,data) => {
+const replacePlaceholder = (obj, data) => {
   Object.keys(obj).forEach(key => {
-  let value = obj[key]
-  if(isString(value)) {
-    value = value.replace(/%(\w+)%/g, function(all, param) {
-      return (data[param] || all);
-    }); 
-    obj[key] = value
-  }
-  if (typeof obj[key] === 'object') {
-    replacePlaceholder(obj[key], data)
-      }
+    let value = obj[key]
+    if (isString(value)) {
+      value = value.replace(/%(\w+)%/g, function (all, param) {
+        return (data[param] || all);
+      });
+      obj[key] = value
+    }
+    if (typeof obj[key] === 'object') {
+      replacePlaceholder(obj[key], data)
+    }
   })
 }
 
-function isString (value) {
+function isString(value) {
   return typeof value === 'string' || value instanceof String;
-  }
+}
