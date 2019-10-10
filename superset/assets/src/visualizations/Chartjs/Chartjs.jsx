3f88f44c689973e58c12358f80c57ed91bdebd1c
jitendra kumawat <jitendra.kumawat@guavus.com>
2019-10-10 17:25:23 +0530

    feat(visualization):added point chart support
    added point chart support
    
    UIC-2127

---
+++ superset/assets/src/visualizations/Chartjs/Chartjs.jsx
@@ -0,0 +1,152 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/* eslint-disable no-param-reassign */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import 'chart.js';
+import 'chartjs-plugin-annotation';
+import './Chartjs.css'
+
+const propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    data: PropTypes.arrayOf(PropTypes.object),
+    xField:PropTypes.string,
+    yField:PropTypes.string,
+  };
+  const defaultProps = {
+    width: '100%',
+    height: '100%',
+    data: undefined,
+    xField: undefined,
+    yField:undefined,
+  };
+  
+  class ChartJSVis extends React.PureComponent {
+    constructor(props) {
+      super(props);
+    }
+
+    renderChart(){
+        var ctx = document.getElementById('chartjs-line-cvs').getContext('2d');
+        var config = this.createConfig();
+        // console.log(config);
+        new Chart(ctx, config);
+    }
+
+    createConfig(){
+        const {width,height,data,xField,yField} = this.props
+        let labels =[];
+        let chartData = [];
+        data.forEach(element => {
+            labels.push(element[yField]);
+            chartData.push(
+                {
+                    x: element[xField],
+                    y: element[yField]
+                }
+            )
+         
+        });
+        return {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: "",
+                    fill: true,
+                    backgroundColor: 'red',
+                    borderColor: 'red',
+                    pointRadius: 7,
+                    pointHoverRadius: 10,
+                    data: chartData
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'linear',
+                        position: 'bottom',
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                    yAxes: [{
+                        type: 'category',
+                        position: 'left',
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                },
+                responsive: true,
+                showLines: false,
+                title: {
+                    display: true,
+                    text: ''
+                },
+                legend: {
+                    display: false
+                },
+                elements: {
+                    point: {
+                        pointStyle: 'circle',
+                    }
+                },
+                annotation: {
+                    annotations: [
+                        {
+                            drawTime: "afterDatasetsDraw",
+                            id: "hline",
+                            type: "line",
+                            mode: "vertical",
+                            scaleID: "x-axis-0",
+                            value: 1,
+                            borderColor: "black",
+                            borderWidth: 3,
+                        }
+
+                    ]
+                }
+
+            },
+
+        }
+    }
+    
+    render() {
+        let style = {display: 'block',height:this.props.height,width:this.props.width}
+        return (
+            <div style ={style}>
+                <canvas id="chartjs-line-cvs" class="chartjs-render-monitor"
+                   style={style}></canvas>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.renderChart();
+    }
+}
+
+ChartJSVis.propTypes = propTypes;
+ChartJSVis.defaultProps = defaultProps;
+
+export default ChartJSVis;
